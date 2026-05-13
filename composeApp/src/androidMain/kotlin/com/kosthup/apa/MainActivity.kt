d65409b51238f1a08d2e8263aca10f8b23a09a05
qoriib <nashrullah.122140162@student.itera.ac.id>
2026-05-13 16:17:35 +0700

    Refactor: Remove HomeScreen and HomeViewModel implementations

--- composeApp/src/androidMain/kotlin/com/kosthup/apa/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/kosthup/apa/MainActivity.kt
@@ -1,22 +1,14 @@
-package com.example.noteai
+package com.kosthup.apa
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 
-/**
- * Android MainActivity
- * 
- * Entry point untuk Android app.
- */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
-        // Enable edge-to-edge display
         enableEdgeToEdge()
-        
         setContent {
             App()
         }

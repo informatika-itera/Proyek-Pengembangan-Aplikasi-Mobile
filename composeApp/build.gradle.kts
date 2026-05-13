d1d01a7862b50643730566162d90ad1a315fc36e
muhammad-nurikhsan <muhammad.123140057@student.itera.ac.id>
2026-05-13 17:21:53 +0700

    chore: rename database and update package name to raillog

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -106,11 +106,11 @@ kotlin {
 }
 
 android {
-    namespace = "com.example.noteai"
+    namespace = "com.example.raillog"
     compileSdk = 35
     
     defaultConfig {
-        applicationId = "com.example.noteai"
+        applicationId = "com.example.raillog"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
@@ -152,8 +152,8 @@ android {
 
 sqldelight {
     databases {
-        create("NoteDatabase") {
-            packageName.set("com.example.noteai.data.local")
+        create("RailLogDatabase") {
+            packageName.set("com.example.raillog.data.local")
         }
     }
 }

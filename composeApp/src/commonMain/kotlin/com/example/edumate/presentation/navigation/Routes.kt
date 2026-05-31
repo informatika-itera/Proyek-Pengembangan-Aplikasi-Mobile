a0f00eb759b5b3bd344de58a19dd83e20a806183
Ghama <ghamaalfajri@gmail.com>
2026-05-31 11:46:32 +0700

    Menambahkan halaman settings

--- composeApp/src/commonMain/kotlin/com/example/edumate/presentation/navigation/Routes.kt
+++ composeApp/src/commonMain/kotlin/com/example/edumate/presentation/navigation/Routes.kt
@@ -3,26 +3,13 @@ package com.example.edumate.presentation.navigation
 import kotlinx.serialization.Serializable
 
 sealed interface Route {
-    @Serializable
-    data object Home : Route
-
-    @Serializable
-    data class AddEditTask(val taskId: Long? = null) : Route
-
-    @Serializable
-    data class TaskDetail(val taskId: Long) : Route
-
-    @Serializable
-    data class AIAssistant(
-        val taskId: Long? = null,
-        val initialText: String? = null
-    ) : Route
-
-    @Serializable
-    data object FocusTimer : Route
-
-    @Serializable
-    data object Statistics : Route
+    @Serializable data object Home : Route
+    @Serializable data class AddEditTask(val taskId: Long? = null) : Route
+    @Serializable data class TaskDetail(val taskId: Long) : Route
+    @Serializable data class AIAssistant(val taskId: Long? = null, val initialText: String? = null) : Route
+    @Serializable data object FocusTimer : Route
+    @Serializable data object Statistics : Route
+    @Serializable data object Settings : Route // Rute Baru
 }
 
 interface NavigationActions {
@@ -32,5 +19,6 @@ interface NavigationActions {
     fun navigateToAIAssistant(taskId: Long? = null, initialText: String? = null)
     fun navigateToFocusTimer()
     fun navigateToStatistics()
+    fun navigateToSettings() // Navigasi Baru
     fun navigateBack()
 }
